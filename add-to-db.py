57edac77e5b5d1bde4a72f191656ca757f3f9f17
Maria Prager <maria.prager@scilifelab.se>
2020-10-06 13:03:55 +0200

    Change interpreter to python3

--- add-to-db.py
+++ add-to-db.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 '''
 Code for reading ASV data and metadata in Excel sheets or *.tsv files,
--- empty-db.py
+++ empty-db.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 '''
 Code for deleting all records in ASV db, and restarting sequences at 1.
